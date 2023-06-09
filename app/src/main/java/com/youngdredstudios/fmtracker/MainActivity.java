a22f5967f0fdb8b2d891ed7c8aae88872c9f0234
DiegoRojo99 <diego99rojo@gmail.com>
2023-06-09 12:11:59 +0100

    Cabinet view implemented
    Now the main activity has a button that goes to cabinet
    
    Cabinet shows an empty trophy card

--- app/src/main/java/com/youngdredstudios/fmtracker/MainActivity.java
+++ app/src/main/java/com/youngdredstudios/fmtracker/MainActivity.java
@@ -2,13 +2,34 @@ package com.youngdredstudios.fmtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button cabinetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cabinetButton = findViewById(R.id.cabinet_button);
+        cabinetButton.setOnClickListener(this);
+    }
+
+
+    public void goToCabinet(){
+        Intent cabinetIntent = new Intent(getApplicationContext(), TrophyCabinet.class);
+        startActivity(cabinetIntent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.cabinet_button){
+            goToCabinet();
+        }
     }
 }
